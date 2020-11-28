006c213a11ecdfc4e360fdf7ba637db7a6b33cfd
jemimaabu <jemimaabu@gmail.com>
2020-11-28 10:42:36 +0200

    updated script

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,26 @@
 /** Handle menu scrolling */
 const menu = document.getElementById("menu");
+const title = document.getElementById("title");
 
 function handleMenuScroll() {
   if (document.body.scrollTop > menu.clientHeight || document.documentElement.scrollTop > menu.clientHeight) {
-    menu.classList.add("scrolled")
+		menu.classList.add("scrolled")
   } else {
-    menu.classList.remove("scrolled")
+		menu.classList.remove("scrolled")
   }
 };
+
+const handleTitleFade = () => {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+		title.classList.add("fade-out")
+  } else {
+		title.classList.remove("fade-out")
+  }
+}
 
 var timer;
 
 window.addEventListener("scroll", function() {
-	if(timer) {
-		window.clearTimeout(timer);
-	}
-
-	timer = window.setTimeout(function() {
-		handleMenuScroll()
-	}, 25);
+	handleMenuScroll();
+	handleTitleFade()
 });
